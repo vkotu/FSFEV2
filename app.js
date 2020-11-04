7264208019c538f2d09e0af839d12b5256d3b684
Venkat kotu <vkotu@paypal.com>
2020-11-04 10:28:26 -0800

    set custom heeader

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
 	res.send('hello world!!');
 });
 
+app.get('/demo', (req, res) => {
+	res.setHeader('X-full-stack', '4life');
+	res.status(418);
+	res.send('I prefer coffee')
+});
+
 app.listen(port, () => console.log('Example app listening on port ${port}!'));
